c430a73b37d90a8ada69fd424bd625e319732461
Kuresu Sahu <kuresusahu18@gmail.com>
2026-03-14 17:14:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(cors())
+app.use(cors({
+  origin: "https://sharemyfile.onrender.com"
+}));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
